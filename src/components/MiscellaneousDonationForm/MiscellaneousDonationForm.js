3a2e71cdf12262e3e802d493f3e1761cbd8cc6fd
James300 <jamesstewart94.300@gmail.com>
2020-02-09 21:20:55 -0600

    Miscellaneous Donation Form (#27)

---
+++ src/components/MiscellaneousDonationForm/MiscellaneousDonationForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Input, Label, TextArea } from 'pcln-design-system';
+import Form from 'components/Form';
+
+function MiscellaneousDonationForm ({
+    className,
+    name,
+    description,
+    quantity,
+    notes,
+    onNameChange,
+    onDescriptionChange,
+    onQuantityChange,
+    onNotesChange,
+}) {
+    return (
+        <Form className={className}>
+            <Input id='name' placeholder='Name' value={name} onChange={onNameChange} />
+            <Input id='description' placeholder='Description' value={description} onChange={onDescriptionChange} />
+            <Input id='quantity' placeholder='Quantity' value={quantity} onChange={onQuantityChange} />
+            <Label>
+                Notes
+                <TextArea id='notes' value={notes} onChange={onNotesChange} />
+            </Label>
+        </Form>
+    );
+}
+
+MiscellaneousDonationForm.displayName = 'MiscellaneousDonationForm';
+
+MiscellaneousDonationForm.propTypes = {
+    className: PropTypes.string,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    quantity: PropTypes.number,
+    notes: PropTypes.string,
+    onNameChange: PropTypes.func,
+    onDescriptionChange: PropTypes.func,
+    onQuantityChange: PropTypes.func,
+    onNotesChange: PropTypes.func,
+};
+
+MiscellaneousDonationForm.defaultProps = {
+    className: '',
+};
+
+export default MiscellaneousDonationForm;
