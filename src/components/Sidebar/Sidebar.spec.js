316a89fae7627c9615aa9d89e6f4779d818ce0d8
James300 <jamesstewart94.300@gmail.com>
2020-06-03 23:29:56 -0500

    Fix Broken Tests

--- src/components/Sidebar/Sidebar.spec.js
+++ src/components/Sidebar/Sidebar.spec.js
@@ -3,7 +3,7 @@ import { fireEvent } from '@testing-library/react';
 import renderWithThemeAndRouter from 'tests/helpers/renderWithThemeAndRouter';
 import Sidebar from './Sidebar';
 import routes from 'constants/routes';
-import { LANDING_PATH, PRODUCTS_PATH, DONORS_PATH } from '../../constants/paths';
+import { LANDING_PATH, DONORS_PATH } from 'constants/paths';
 
 describe ('Sidebar', () => {
     it ('renders without crashing', () => {
@@ -24,17 +24,6 @@ describe ('Sidebar', () => {
         );
 
         expect(asFragment()).toMatchSnapshot();
-    });
-
-    it ('can navigate to products page', () => {
-        const { getByText, history } = renderWithThemeAndRouter(
-            <Sidebar currentPathname={LANDING_PATH} routes={routes} />
-        );
-        const productsButton = getByText('Products');
-
-        expect(history.location.pathname).toBe(LANDING_PATH);
-        fireEvent.click(productsButton);
-        expect(history.location.pathname).toBe(PRODUCTS_PATH);
     });
 
     it ('can navigate to donors page', () => {
