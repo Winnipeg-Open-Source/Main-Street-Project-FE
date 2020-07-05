9eaee6200698e0ac75eddb1b442b6cbf2b0b5a9d
James300 <jamesstewart94.300@gmail.com>
2020-07-05 00:25:27 -0500

    Locations Page - Not Found State (#82)

--- src/pages/Locations/Locations.js
+++ src/pages/Locations/Locations.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import Page from 'components/Page';
+import Input from 'components/Input';
 import LocationsList from 'containers/LocationsList';
 import NewButton from 'containers/NewButton';
 import useLocations from 'hooks/context/useLocations';
 
 function Locations () {
-    const { isLoading, onLoadLocations } = useLocations();
+    const { isLoading, locations, onLoadLocations } = useLocations();
+
+    const disabled = isLoading || (locations && locations.length === 0);
 
     useEffect(() => {
         isLoading && onLoadLocations();
@@ -13,6 +16,7 @@ function Locations () {
 
     return (
         <Page title='Locations' renderAction={NewButton}>
+            <Input id='search' disabled={disabled} placeholder='Search' mb={3} />
             <LocationsList />
         </Page>
     );
