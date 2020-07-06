2ccf16db051b940c5b622790bd6aae8abbc898ed
James300 <jamesstewart94.300@gmail.com>
2020-07-06 00:36:47 -0500

    Items Page - Not Found State

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,18 +1,22 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
+import { Text } from 'pcln-design-system';
 import List from 'components/List';
 import ItemCard from 'components/ItemCard';
 
-function ItemList ({ className, items, onItemQuantityChange }) {
+function ItemList ({ className, showEmpty, items, onItemQuantityChange }) {
     return (
         <List className={className}>
-            {items && items.map(item => (
-                <ItemCard
-                    key={item.id}
-                    {...item}
-                    onItemQuantityChange={onItemQuantityChange}
-                />
-            ))}
+            {items && items.length > 0
+                ? items.map(item => (
+                    <ItemCard
+                        key={item.id}
+                        {...item}
+                        onItemQuantityChange={onItemQuantityChange}
+                    />
+                ))
+                : showEmpty && <Text textAlign='center'>No items yet...</Text>
+            }
         </List>
     );
 }
@@ -21,6 +25,7 @@ ItemList.displayName = 'ItemList';
 
 ItemList.propTypes = {
     className: PropTypes.string,
+    showEmpty: PropTypes.bool,
     items: PropTypes.array,
     onItemQuantityChange: PropTypes.func,
 };
--- src/pages/DonationCreate/DonationCreate.spec.js
+++ src/pages/DonationCreate/DonationCreate.spec.js
@@ -2,28 +2,36 @@ import React from 'react';
 import renderWithThemeAndRouter from 'tests/helpers/renderWithThemeAndRouter';
 import DonationCreate from './DonationCreate';
 import useDonors from 'hooks/context/useDonors';
+import useItems from 'hooks/context/useItems';
 
 jest.mock('hooks/context/useDonors');
+jest.mock('hooks/context/useItems');
 
 describe ('DonationCreate', () => {
     it ('renders without crashing', () => {
         const mockLoadDonors = jest.fn();
+        const mockLoadItems = jest.fn();
 
         useDonors.mockImplementation(() => ({ isLoading: true, onLoadDonors: mockLoadDonors }));
+        useItems.mockImplementation(() => ({ isLoading: true, onLoadItems: mockLoadItems }));
 
         const { asFragment } = renderWithThemeAndRouter(<DonationCreate />);
 
         expect(mockLoadDonors).toHaveBeenCalledTimes(1);
+        expect(mockLoadItems).toHaveBeenCalledTimes(1);
         expect(asFragment()).toMatchSnapshot();
     });
 
     it ('does not load already loaded resources', () => {
         const mockLoadDonors = jest.fn();
+        const mockLoadItems = jest.fn();
 
         useDonors.mockImplementation(() => ({ isLoading: false, onLoadDonors: mockLoadDonors }));
+        useItems.mockImplementation(() => ({ isLoading: false, onLoadItems: mockLoadItems }));
 
         renderWithThemeAndRouter(<DonationCreate />);
 
         expect(mockLoadDonors).toHaveBeenCalledTimes(0);
+        expect(mockLoadItems).toHaveBeenCalledTimes(0);
     });
 });
