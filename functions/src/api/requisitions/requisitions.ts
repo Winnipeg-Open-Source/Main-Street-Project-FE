e1076d3512533b61e7ff31162a6b7c007df1269a
James300 <jamesstewart94.300@gmail.com>
2020-07-07 00:56:48 -0500

    Requisitions get and getAll APIs

--- functions/src/api/requisitions/requisitions.ts
+++ functions/src/api/requisitions/requisitions.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { REQUISITIONS_COLLECTION } from '../../constants/collections';
 import { update as updateLineItems } from '../../services/lineItems';
-import { create } from '../../utils/firebase';
+import { create, get, getAll } from '../../utils/firebase';
 
 const router = express.Router();
 
@@ -13,6 +13,21 @@ router.post('/', async (req: any, res: any) => {
 
     res.status(201);
     res.json(requisition);
+});
+
+router.get('/:id', async (req: any, res: any) => {
+    const id: string = req.params.id;
+    const requisition = await get(REQUISITIONS_COLLECTION, id);
+
+    res.status(200);
+    res.json(requisition);
+});
+
+router.get('/', async (req: any, res: any) => {
+    const requisitions = await getAll(REQUISITIONS_COLLECTION);
+
+    res.status(200);
+    res.json(requisitions);
 });
 
 export default router;
