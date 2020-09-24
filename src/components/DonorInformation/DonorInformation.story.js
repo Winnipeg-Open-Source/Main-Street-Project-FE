060dbb71208179480016184e0afc05d25a1c34e3
James300 <jamesstewart94.300@gmail.com>
2020-09-24 00:41:18 -0500

    Fix Tests

--- src/components/DonorInformation/DonorInformation.story.js
+++ src/components/DonorInformation/DonorInformation.story.js
@@ -3,15 +3,17 @@ import DonorInformation from './DonorInformation';
 import DonorInformationSkeleton from './DonorInformationSkeleton';
 
 const props = {
+    name: 'Costco',
     address: '2600 - 360 Main Street',
-    city: 'Winnipeg, MB',
+    city: 'Winnipeg',
+    province: 'MB',
     email: 'test@gmail.com',
     phoneNumber: '204-555-5555',
 };
 
 export const Loading = () => <DonorInformationSkeleton />;
 
-export const WithoutInfo = () => <DonorInformation />;
+export const WithoutProvince = () => <DonorInformation {...props} province={null} />;
 
 export const WithInfo = () => <DonorInformation {...props} />;
 
